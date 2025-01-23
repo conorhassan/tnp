c26b74bcaec10c6ba3a87ed388b26a2a7bee6220
Conor Hassan <conor@Conors-MacBook-Pro.local>
2025-01-23 21:27:11 +0200

    moved all the hacky test scripts to the  folder

--- tnp/test_np_nnx.py
+++ tnp/test_np_nnx.py
@@ -5,22 +5,10 @@ import optax
 from typing import Tuple
 from collections import deque
 
-from tnp.data.gp_redo import SimpleGPGenerator
-from tnp.models.nnx_models.layers import TransformerBlock, TNPTransformer, MLP
-from tnp.models.nnx_models.neural_process import TNPEncoder, TNPDecoder, TNP 
+from tnp.data.simple_gp import SimpleGPGenerator
+from tnp.models.nnx_models.layers import TNPTransformer, MLP
+from tnp.models.nnx_models.neural_process import TNP 
 from tnp.models.nnx_models.likelihood import HeteroscedasticNormalLikelihood
-
-transformer_encoder = nnx.MultiHeadAttention(
-    num_heads=4, 
-    in_features=128, 
-    qkv_features=16, 
-    out_features=16, 
-    dropout_rate=0.2,
-    deterministic=False,
-    use_bias=True,
-    decode=False,
-    rngs=nnx.Rngs(0)
-)
 
 def create_model_and_optimizer(
     input_dim: int, 
@@ -63,46 +51,6 @@ def create_model_and_optimizer(
     return model, optimizer
 
 if __name__ == "__main__":
-    batch_dim = 5
-    seq_dim = 10 
-    input_dim = 128 
-
-    key1, key2, key3 = jax.random.split(jax.random.PRNGKey(0), 3)
-
-    q, k, v = (
-        jax.random.normal(key1, (batch_dim, seq_dim, input_dim)),
-        jax.random.normal(key2, (batch_dim, seq_dim, input_dim)),
-        jax.random.normal(key3, (batch_dim, seq_dim, input_dim)),
-    )
-
-    print("TESTING MULTIHEAD ATTENTION LAYER")
-    print(transformer_encoder(q, k, v))
-
-    print("TESTING TRANSFORMER BLOCK")
-    attention_block = TransformerBlock(
-        input_dim=input_dim,
-        hidden_dim=input_dim,
-        out_dim=input_dim,
-        num_heads=4,
-        rngs=nnx.Rngs(0),
-    )
-
-    print(attention_block(q).shape)
-    
-    print("TESTING THE TRANSFORMER NEURAL PROCESS")
-    tnp_transformer = TNPTransformer(
-        input_dim=64,
-        hidden_dim=64,
-        num_heads=4,
-        dropout_rate=0.2,
-        rngs=nnx.Rngs(0),
-    )
-
-    zc = jnp.ones((512, 10, 64))
-    zt = jnp.ones((512, 7, 64))
-
-    print(jnp.concatenate([zc, zt], axis=1).shape)
-    print(tnp_transformer(zc, zt).shape)
 
     model, optimizer = create_model_and_optimizer(
         input_dim=1,
@@ -139,6 +87,3 @@ if __name__ == "__main__":
         avg_loss = sum(losses) / len(losses)
         print(f"Step {i}, Loss: {avg_loss:.4f}")
     print("MODEL RUNS THE FORWARD METHOD MULTIPLE TIMES WITH NO PROBLEMS!")
-
-    import code
-    code.interact(local=locals())
