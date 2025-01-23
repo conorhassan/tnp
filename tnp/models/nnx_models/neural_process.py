a2e667bcc3276f0a54581704c4e2d3db0e3dfaf8
Conor Hassan <conor@Conors-MacBook-Pro.local>
2025-01-23 17:24:43 +0200

    finished adapting np nnx to handle batch dimension

--- tnp/models/nnx_models/neural_process.py
+++ tnp/models/nnx_models/neural_process.py
@@ -6,8 +6,9 @@ import numpyro.distributions as dist
 from jaxtyping import Array, Float
 from typing import Tuple, Optional
 from einops import pack, unpack, rearrange
-from nnx.layers import Identity
 
+from nnx_models.layers import Identity
+from nnx_models.likelihood import Likelihood
 
 class TNPEncoder(nnx.Module):
     def __init__(
@@ -23,39 +24,41 @@ class TNPEncoder(nnx.Module):
         self.y_encoder = y_encoder
 
     def __call__(
-        self, xc: Float[Array, "..."], 
-        yc: Float[Array, "..."], 
-        xt: Float[Array, "..."], 
-        key: jax.random.PRNGKey, 
-        enable_dropout: bool = False
-    ) -> Float[Array, "num_target latent_dim"]:
+        self, 
+        xc: Float[Array, "batch num_context input_dim"], 
+        yc: Float[Array, "batch num_context input_dim"], 
+        xt: Float[Array, "batch num_target output_dim"],
+    ) -> Float[Array, "batch num_target latent_dim"]:
         yc, yt = self.preprocess_observations(xt, yc)
 
-        x, ps = pack([xc, xt], "* d")
+        x, ps = pack([xc, xt], "batch (context_and_target) features")
         x_encoded = self.x_encoder(x)
-        xc_encoded, xt_encoded = unpack(x_encoded, ps, "* d")
+        xc_encoded, xt_encoded = unpack(x_encoded, ps, "batch (context_and_target) features") 
 
-        y, ps = pack([yc, yt], "* d")
+        y, ps = pack([yc, yt], "batch (context_and_target) features")
         y_encoded = self.y_encoder(y)
-        yc_encoded, yt_encoded = unpack(y_encoded, ps, "* d")
+        yc_encoded, yt_encoded = unpack(y_encoded, ps, "batch (context_and_target) features")
 
-        zc, _ = pack([xc_encoded, yc_encoded], "s *")
-        zt, _ = pack([xt_encoded, yt_encoded], "s *")
+        zc, _ = pack([xc_encoded, yc_encoded], "batch seq (d1 d2)")
+        zt, _ = pack([xt_encoded, yt_encoded], "batch seq (d1 d2)")
 
         zc = self.xy_encoder(zc)
         zt = self.xy_encoder(zt)
 
-        output = self.transformer_encoder(zc, zt, key=key, enable_dropout=enable_dropout)
-        return output
+        return self.transformer_encoder(zc, zt)
     
     def preprocess_observations(
         self, 
-        xt: Float[Array, "num_target input_dim"], 
-        yc: Float[Array, "num_context output_dim"]
-    ) -> Tuple[Float[Array, "num_target output_dim_plus_1"]]:
-        yt = jnp.zeros((xt.shape[0], yc.shape[-1]))
-        yc = jnp.concatenate([yc, jnp.zeros_like(yc[..., :1])], axis=-1)
+        xt: Float[Array, "batch num_target input_dim"], 
+        yc: Float[Array, "batch num_context output_dim"]
+    ) -> Tuple[Float[Array, "batch num_target output_dim_plus_1"]]:
+        # create zero tensor for the target points, since they are unknown 
+        yt = jnp.zeros((*xt.shape[:-1], yc.shape[-1]))
+
+        # add flag dimensions
+        yc = jnp.concatenate([yc, jnp.zeros_like(yc[..., :1])], axis=-1) # add a zero flag 
         yt = jnp.concatenate([yt, jnp.ones_like(yt[..., :1])], axis=-1)
+
         return yc, yt
 
 
@@ -65,12 +68,12 @@ class TNPDecoder(nnx.Module):
 
     def __call__(
         self, 
-        z: Float[Array, "num_target latent_dim"], 
-        xt: Optional[Float[Array, "num_target input_dim"]] = None
-    ) -> Float[Array, "num_target output_dim"]:
+        z: Float[Array, "batch num_target latent_dim"], 
+        xt: Optional[Float[Array, "batch num_target input_dim"]] = None
+    ) -> Float[Array, "batch num_target output_dim"]:
         if xt is not None:
             num_target = xt.shape[0]
-            zt = rearrange(z[-num_target:, :], "t d -> t d")
+            zt = rearrange(z[-num_target:, :], "b t d -> b t d")
         else: 
             zt = z
         return self.z_decoder(zt)
@@ -109,4 +112,4 @@ class TNP(ConditionalNeuralProcess):
         key: jax.random.PRNGKey, 
         enable_dropout: bool = False
     ) -> dist.Distribution:
-        return super().__call__(xc, yc, xt, key, enable_dropout)
+        return super().__call__(xc, yc, xt)
